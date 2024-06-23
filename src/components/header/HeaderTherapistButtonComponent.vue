dd23bcc13409cc47a211e02408dcdb23fa5205bf
mudmykola <mykola.mud@gmail.com>
2024-06-23 20:34:59 +0300

    style(Navigation): added styles for btn therapist

--- src/components/header/HeaderTherapistButtonComponent.vue
+++ src/components/header/HeaderTherapistButtonComponent.vue
@@ -1,7 +1,22 @@
 <script setup>
+import { defineProps } from 'vue';
 
+const props = defineProps({
+  btnText: {
+    type: String,
+    required: true,
+    default: 'Підібрати психотерапевта',
+  },
+  btnUrl: {
+    type: String,
+    required: true,
+    default: '#',
+  }
+})
 </script>
 
 <template>
-Therapist Button
+<div class="therapist">
+  <a class="therapist-link bg-primary rounded-[12px] p-[18px] text-white font-sans font-bold text-[14px]" :href="btnUrl">{{ btnText }}</a>
+</div>
 </template>
