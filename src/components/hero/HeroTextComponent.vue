c5fcc17caa9de97f55cfd5ddcb019678b1c14b5a
mudmykola <mykola.mud@gmail.com>
2024-06-28 08:27:23 +0300

    style(Hero): added colours for options

--- src/components/hero/HeroTextComponent.vue
+++ src/components/hero/HeroTextComponent.vue
@@ -9,7 +9,7 @@ const therapistList = ref([
   { name: 'Психологи', link: '' },
   { name: 'Психотерапевти', link: '' },
   { name: 'Психіатри', link: '' },
-  { name: 'Онлайн', link: '' },
+  { name: 'Онлайн', link: '',},
   { name: 'Офлайн', link: '' }
 ]);
 
@@ -43,7 +43,10 @@ const toggleTherapistList = () => {
       </div>
       <div class="hero-text__view--therapist__list " v-if="showTherapistList">
         <ul class="flex items-center flex-wrap gap-2  w-[490px]">
-          <li class="bg-[#DEE2DE] border border-primary border-opacity-40 py-[10px] px-[14px] rounded-[111px]  transition-all duration-300" v-for="therapist in therapistList" :key="therapist.name">
+          <li class="bg-[#DEE2DE] border border-primary border-opacity-40 py-[10px] px-[14px] rounded-[111px]  transition-all duration-300 hover:bg-accent hover:bg-opacity-40"
+              v-for="therapist in therapistList"
+              :key="therapist.name"
+          >
             <a class="text-[16px] font-normal font-sans text-primary" :href="therapist.link">{{ therapist.name }}</a>
           </li>
         </ul>
