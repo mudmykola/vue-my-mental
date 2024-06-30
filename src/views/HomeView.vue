3889613b813dacf485fb891b1fe450f425f86394
mudmykola <mykola.mud@gmail.com>
2024-06-30 09:31:50 +0300

    feat(Manual): added a block with benefits

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import DefaultLayout from "@/layout/DefaultLayout.vue";
 import HeroComponent from "@/components/hero/HeroComponent.vue";
+import ManualComponent from "@/components/manual/ManualComponent.vue";
 </script>
 
 <template>
   <DefaultLayout>
     <HeroComponent/>
+    <ManualComponent/>
   </DefaultLayout>
 </template>
