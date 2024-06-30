3007b9ac560cebbcce0428e640c871efbfb737f4
mudmykola <mykola.mud@gmail.com>
2024-06-30 12:22:34 +0300

    feat(Manual): created a reusable component with text and a picture

--- src/components/manual/ManualComponent.vue
+++ src/components/manual/ManualComponent.vue
@@ -1,26 +1,31 @@
 <script setup>
-
+import { ref } from "vue";
 import ManualStatisticsComponent from "@/components/manual/ManualStatisticsComponent.vue";
+import ManualTextImageLeftComponent from "@/components/manual/ManualTextImageComponent.vue";
+
+const manualTitle = ref('Mymental - це зручно:');
 </script>
 
 <template>
-<div class="manual bg-muted h-[100vh ">
-  <div class="container">
-    <div class="manual-inner relative">
-      <div class="manual-statistics absolute right-0 left-0  bottom-[50%]">
-        <ManualStatisticsComponent />
+  <div class="manual bg-muted h-full relative">
+    <div class="container">
+      <div class="manual-inner ">
+        <div class="manual-statistics container absolute right-0 left-0 bottom-[50%] top-[-9%]">
+          <ManualStatisticsComponent/>
+        </div>
+        <div class="manual-inner__title text-center pt-[154px]">
+          <h2 class="text-[48px] font-bold font-sans text-black">{{ manualTitle }}</h2>
+        </div>
+        <div class="manual-test mt-12">
+          <ManualTextImageLeftComponent
+              img-alt="left-image"
+              img-src="/images/manual-test-image.png"
+              text-desc="пройди швидкий тест або обери з каталогу"
+              text-title="Обери психотерапевта"
+              :reverse="false"
+          />
+        </div>
       </div>
-      <div class="manual-test pt-[154px]">
-        hello
-      </div>
-
     </div>
-
   </div>
-
-</div>
 </template>
-
-<style scoped lang="scss">
-
-</style>
