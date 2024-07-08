ffabfec149aca2bb6d8d3deaf0986393f7615962
mudmykola <mykola.mud@gmail.com>
2024-07-08 09:07:19 +0300

    feat(Specialists): Introduced cards with benefits

--- src/components/specialists/SpecialistsCartBenefitsComponent.vue
+++ src/components/specialists/SpecialistsCartBenefitsComponent.vue
@@ -1,19 +1,40 @@
 <script setup>
 import IconMessage from "@/components/icons/IconMessage.vue";
+import IconScience from "@/components/icons/IconScience.vue";
+import IconTeam from "@/components/icons/IconTeam.vue";
+import IconTechnology from "@/components/icons/IconTechnology.vue";
+import { ref } from "vue";
+
+const benefits = ref([
+	{ text: "Перевірені експерти", icon: IconMessage },
+	{
+		text: "Мають власний досвід проходження довгострокової індивідуальної психотерапії",
+		icon: IconTeam,
+	},
+	{
+		text: "Всі спеціалісти мають вищу психологічну або медичну освіту",
+		icon: IconTechnology,
+	},
+	{
+		text: "Регулярно проходять супервізію\n" + "для підвищення кваліфікації",
+		icon: IconScience,
+	},
+]);
 </script>
 
 <template>
   <div class="specialists-cart__benefit--list">
-    <ul>
-      <li>
-        <div class="specialists-cart__benefit--box">
-          <IconMessage />
-          <p>Мають досвід роботи
-            від 2-х років</p>
+    <ul class="flex gap-4 w-[800px] flex-wrap">
+      <li
+        v-for="(benefit, index) in benefits"
+        :key="index"
+        class="bg-white rounded-xl shadow-2xl w-[379px] mb-4 hover:bg-muted hover:transition duration-300 cursor-pointer"
+      >
+        <div class="specialists-cart__benefit--box py-8 px-8">
+          <component :is="benefit.icon" />
+          <p class="mt-6 text-[16px] text-text">{{ benefit.text }}</p>
         </div>
       </li>
     </ul>
   </div>
 </template>
-
-<style lang="scss" scoped></style>
