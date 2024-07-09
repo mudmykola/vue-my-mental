5d2c42ce7a59a9b5b9582237c82f9a12bb6f2390
mudmykola <mykola.mud@gmail.com>
2024-07-09 08:19:08 +0300

    feat(Specialists): added cards with benefitss

---
+++ src/components/specialists/SpecialistsCartContentComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import specialistsContImage from "/images/specialists-cont-image.png";
+
+const props = defineProps({
+	altText: {
+		type: String,
+		required: true,
+		default: "Hero Image",
+	},
+});
+
+const specialistCartText =
+	"Пройшли тривале\n" +
+	"фундаментальне навчання\n" +
+	"в одному або більше\n" +
+	"психотерапевтичних методів";
+</script>
+
+<template>
+  <div class="specialists-cart__content  shadow-2xl p-8 rounded-xl hover:bg-muted hover:transition duration-300 cursor-pointer">
+    <img :alt="altText" :src="specialistsContImage" />
+    <p class="pt-8 w-[225px] text-[16px] text-text">{{ specialistCartText }}</p>
+  </div>
+</template>
