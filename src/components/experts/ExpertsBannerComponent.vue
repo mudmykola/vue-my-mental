f4f573f42d642180555aa6a5a62414cbcf2d2935
mudmykola <mykola.mud@gmail.com>
2024-08-10 10:22:23 +0300

    feat(Experts): added decorative elements

--- src/components/experts/ExpertsBannerComponent.vue
+++ src/components/experts/ExpertsBannerComponent.vue
@@ -14,7 +14,7 @@ const expertsFilterResult = ref("По вашому запиту знайдено
 </script>
 
 <template>
-  <div class="experts-banner bg-white rounded-[32px]">
+  <div class="experts-banner bg-white rounded-[32px] mb-8">
     <div class="experts-banner__inner py-9 text-center">
       <div class="experts-breadcrumb flex justify-center mx-auto mb-3">
         <Breadcrumbs />
@@ -29,3 +29,21 @@ const expertsFilterResult = ref("По вашому запиту знайдено
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.experts-banner {
+  position: relative;
+  &::after{
+    position: absolute;
+    content: '';
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url("/images/tests-element-2.png");
+    background-repeat: no-repeat;
+    -webkit-background-size: contain;
+    background-size: contain;
+  }
+}
+</style>
